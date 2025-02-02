b6628af3fd7a63e40ab4ab09de5400132847c2bc
upeshchalise <upeshchalise@gmail.com>
2025-02-02 17:23:17 +0545

    started making portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/Header";
+import { DropdownMenu } from "@/components/ui/dropdown-menu";
+
 export default function Home() {
   return (
     <div>
+      <Header />
       <p>The name is upesh chalise.</p>
+      <DropdownMenu>{<div>hello</div>} </DropdownMenu>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div>THis is the header</div>;
+};
+
+export default Header;
