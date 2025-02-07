e1e8061d60617d93d9a173eaa734f803f0ddb60d
samir <upeshchalise@gmail.com>
2025-02-07 22:20:47 +0545

    base design --in progress

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = () => {
-  return <div>THis is the header</div>;
+  return (
+    <>
+    <div className="  flex justify-between text-white p-2">
+      <div className="flex gap-5">
+        <div className="text-white">3 dots</div>
+        <div>logo terminal</div>
+        <div>search section</div>
+      </div>
+      <div>github star section</div>
+    </div>
+    </>
+  )
 };
 
 export default Header;
---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,17 @@
+import { Github, House, Linkedin, Mail, Star } from "lucide-react";
+
+export const SideNav = () => {
+  return (
+    <>
+      <div className="text-white pl-3 pt-3 h-full w-16 items-center">
+        <ul className="flex flex-col gap-4">
+            <li><House size={28} strokeWidth={1.25} /></li>
+            <li><Star size={28} strokeWidth={1.25} /></li>
+            <li><Github size={28} strokeWidth={1.25} /></li>
+            <li><Linkedin size={28} strokeWidth={1.25} /></li>
+            <li><Mail size={28} strokeWidth={1.25} /></li>
+        </ul>
+      </div>
+    </>
+  );
+};
